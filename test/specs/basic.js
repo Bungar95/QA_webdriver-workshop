c468e974d8b08460c55965f5883bd5cd4fe0c84d
Borna Ungar <36451381+Bungar95@users.noreply.github.com>
2019-11-28 19:02:12 +0100

    Add files via upload

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,7 +1,13 @@
 const assert = require('assert');
 
-describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://webdriver.io');
-    });
+describe('New webmail page', function(){
+    it("New webmail page", function() {
+        browser.url("https://webmail.ffos.hr/");
+        browser.pause(5000);
+        $("#rcmloginuser").setValue("bungar");
+        $("#rcmloginpwd").setValue("3jPsXdat");
+        $("#rcmloginsubmit").click();
+        browser.pause(5000);
+        browser.getTitle();
+    });   
 });
